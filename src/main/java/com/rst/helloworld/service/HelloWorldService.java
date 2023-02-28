34d93ba7dffeebab82e88b536d139c449f4ca7b5
sekharreddy0463 <53187022+sekharreddy0463@users.noreply.github.com>
2023-02-28 18:41:28 +0530

    Update HelloWorldService.java

--- src/main/java/com/rst/helloworld/service/HelloWorldService.java
+++ src/main/java/com/rst/helloworld/service/HelloWorldService.java
@@ -23,7 +23,7 @@ public class HelloWorldService {
 		logger.debug("getTitle() is executed! $name : {}", name);
 
 		if(StringUtils.isEmpty(name)){
-			return "Hello chandra and Reddy";
+			return "Hello chandu and Reddy";
 		}else{
 			return "Hello " + name;
 		}
