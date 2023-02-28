dc72a3242a14bffe4c42e8da80d46279e4d20e83
sekharreddy0463 <53187022+sekharreddy0463@users.noreply.github.com>
2023-02-28 17:58:42 +0530

    Update HelloWorldService.java

--- src/main/java/com/rst/helloworld/service/HelloWorldService.java
+++ src/main/java/com/rst/helloworld/service/HelloWorldService.java
@@ -23,7 +23,7 @@ public class HelloWorldService {
 		logger.debug("getTitle() is executed! $name : {}", name);
 
 		if(StringUtils.isEmpty(name)){
-			return "Hello Docker and ansible";
+			return "Hello chandra and Reddy";
 		}else{
 			return "Hello " + name;
 		}
